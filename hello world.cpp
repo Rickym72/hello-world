48ecc54e54f56113c11a9b8a9bf59597c735b53a
Ricardo A Martinez <rickymartinez714@gmail.com>
2019-10-04 13:29:39 -0700

    Update hello world.cpp

--- hello world.cpp
+++ hello world.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+helpSayHello(){
+vector<int> i = 10;
+  for(int j=0;j<i.size()-1;j++){
+  cout<< "hello world";
+  }
+}
+
+int main()  
+{
+  helpSayHello();
+return 0;
+}
